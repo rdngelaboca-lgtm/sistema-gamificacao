0bfac9250abc3f283e7e59be8a2bac2749f88f35
rdngelaboca-lgtm <rdngelaboca@gmail.com>
2025-10-11 15:36:24 -0400

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -416,6 +416,7 @@ def rota_painel_tarefas():
 
 if __name__ == '__main__':
     print(">>> Iniciando o Servidor da API...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=false)
+
 
 
